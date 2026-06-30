f5b25f935e9f95ab5097335ecc52c2fc5cbf75d6
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-06-30 18:43:05 -0500

    subiendo Dashboard

---
+++ Proyecto1_Final_POO-main/ProyectoFinalPoo/src/main/java/model/Cita.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Cita {
+    private int id;
+    private int clienteId;
+    private int servicioId;
+    private String fecha;
+    private String hora;
+    private String estado;
+}
+
---
+++ Proyecto1_Final_POO-main/ProyectoFinalPoo/src/main/java/model/Servicio.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Servicio {
+    private int id;
+    private String nombre;
+    private double precio;
+    private String duracion;
+}
+
